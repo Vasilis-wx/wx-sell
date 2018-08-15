43376a33e9fe23d063c790844aa900a3ae0d8258
wangxin <wangxin@wx.com>
2018-08-15 10:16:06 +0800

    新建微信点餐项目

--- src/main/java/com/wx/WxsellApplication.java
+++ src/main/java/com/wx/WxsellApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WxsellApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(WxsellApplication.class, args);
 	}
 }
