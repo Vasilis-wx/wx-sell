35b0badde0fe1c028ffdf034eacc3ecc2d4c91b0
wangxin <wangxin@wx.com>
2018-08-24 16:01:16 +0800

    捕获异常

--- src/main/java/com/wx/handler/SellExceptionHandler.java
+++ src/main/java/com/wx/handler/SellExceptionHandler.java
@@ -1,10 +1,16 @@
 package com.wx.handler;
 
+import com.wx.VO.ResultVO;
 import com.wx.config.ProjectUrlConfig;
+import com.wx.exception.SellException;
 import com.wx.exception.SellerAuthorizeException;
+import com.wx.util.ResultVOUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -17,7 +23,11 @@ public class SellExceptionHandler {
     @Autowired
     private ProjectUrlConfig projectUrlConfig;
 
-    //拦截登录异常
+    /**
+     * 拦截登录异常
+     * 跳转到登录页
+     * @return
+     */
     //http://sell.natapp4.cc/sell/wechat/qrAuthorize?returnUrl=http://sell.natapp4.cc/sell/seller/login
     @ExceptionHandler(value = SellerAuthorizeException.class)
     public ModelAndView handlerAuthorizeException() {
@@ -27,5 +37,16 @@ public class SellExceptionHandler {
         .concat("?returnUrl=")
         .concat(projectUrlConfig.getSell())
         .concat("/sell/seller/login"));
+    }
+
+    /**
+     * 拦截操作异常
+     * @return
+     */
+    @ExceptionHandler(value = SellException.class)
+    @ResponseBody
+//    @ResponseStatus(HttpStatus.FORBIDDEN)//更改返回的http状态码
+    public ResultVO handlerSellException(SellException e){
+        return ResultVOUtil.error(e.getCode(),e.getMessage());
     }
 }
